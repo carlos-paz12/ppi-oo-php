cbcbebc20bdbf1f5268e17a2645a4b16bd94cbf1
Carlos Paz <147278027+carlos-paz12@users.noreply.github.com>
2023-10-13 19:30:22 -0300

    feat: add getters and setter in Pessoa.php

--- Pessoa.php
+++ Pessoa.php
@@ -3,12 +3,59 @@
 /* 
  * Crie uma classe PHP para o tipo Pessoa, com as propriedades:
  * nome, nascimento,  cidade, genero
+ *
+ *
+ * Implemente o conceito de encapsulamento na classe.
  */
-class Pessoa {
-
+class Pessoa
+{
     private $nome;
     private $nascimento;
     private $cidade;
     private $genero;
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getNascimento()
+    {
+        return $this->nascimento;
+    }
+
+    public function setNascimento($nascimento)
+    {
+        $this->nascimento = $nascimento;
+        return $this;
+    }
+
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
+        return $this;
+    }
+
+    public function getGenero()
+    {
+        return $this->genero;
+    }
+
+    public function setGenero($genero)
+    {
+        $this->genero = $genero;
+        return $this;
+    }
 
 }
