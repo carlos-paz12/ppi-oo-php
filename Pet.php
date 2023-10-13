8bef649a9e7a5cadd9dd0135e05c6ae4d0b56829
Carlos Paz <147278027+carlos-paz12@users.noreply.github.com>
2023-10-13 19:18:04 -0300

    feat: create Pet.php

---
+++ Pet.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Crie uma classe PHP para o tipo Pet, com as propriedades:
+ * nome, especie, raca, idade, nomeDono
+ */
+class Pet {
+
+    private $nome;
+    private $especie;
+    private $raca;
+    private $idade;
+    private $nomoDono;
+
+}
